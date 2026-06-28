7b2e40adf620301a448624bca4d197a2a4bbbc4a
jan92106 <janviagrawal921@gmail.com>
2026-06-29 03:23:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -110,3 +110,10 @@ def serve_static(path):
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
+
+
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
